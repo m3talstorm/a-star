baa0cf9209d60d535011d8171aedf56298effcb9
m3talstorm <metalheadstorm@gmail.com>
2018-07-29 20:23:43 +0100

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,18 @@
+
+"""
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='a-star',
+    version='0.1',
+    url='https://github.com/m3talstorm/a-star',
+    download_url='https://github.com/m3talstorm/a-star',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[]
+)
